0ac4f329217429529f952e8d2743bf40930428bd
Menghetz <manuelmenghetti0@gmail.com>
2025-12-07 18:05:33 +0100

    Install some dependecies

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express"
+
+const app: Express = express()
+const port: number = 3000
+
+app.use(express.static('dist'))
+
+app.use(express.static("public"))
+app.use(express.static("dist-frontend"))
+
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "text/plain")
+  res.status(404).send("Ops... Pagina non trovata")
+})
+
+app.listen(port, function() {
+  console.log(`Listening on http://localhost:${port}`)
+})
