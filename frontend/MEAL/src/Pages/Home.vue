aa2a10a0f6045ffa8eb6cefd8d5bef73dc0d80bf
evampa <evampa@imolinfo.it>
2025-12-08 18:04:20 +0100

    aggiunta search bar

--- frontend/MEAL/src/Pages/Home.vue
+++ frontend/MEAL/src/Pages/Home.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+    import SearchBar from '../components/SearchBar.vue'
+</script>
+
 <template>
-    <h1>Welcome to MEAL</h1>
+  <div class="home-page">
+    <SearchBar/>
+  </div>
 </template>
---
+++ frontend/MEAL/src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input
+      v-model="searchTerm"
+      type="text"
+      placeholder="Cerca..."
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const searchTerm = ref('')
+
+watch(searchTerm, (newVal, oldVal) => {
+  if (newVal.length > oldVal.length) {
+    console.log('Contenuto input:', newVal)
+  }
+})
+</script>
