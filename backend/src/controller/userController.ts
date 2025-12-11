7e8ce63265b4ccba2ab67c50345d6364318d0bd3
Menghetz <manuelmenghetti0@gmail.com>
2025-12-11 15:34:44 +0100

    minor fixes

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -5,7 +5,7 @@ export async function userByID(req: Request, res: Response) {
     connection.execute(
         `SELECT ID, immagine
          FROM utente
-         where ID = ?`,
+         where ID like ?`,
          [req.params.ID],
          function(err, results, fields) {
             res.json(results)
