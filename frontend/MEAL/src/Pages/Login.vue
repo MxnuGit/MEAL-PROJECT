dbbc3d695ee9514d7f3aedb22be53ee270a8f46a
Fabian <fabiancalangiu5@gmail.com>
2025-12-12 17:22:22 +0100

    Added Login page minimal logic

--- frontend/MEAL/src/Pages/Login.vue
+++ frontend/MEAL/src/Pages/Login.vue
@@ -1,34 +1,75 @@
 <template>
     <div>
         <header>
-            <h1>Login</h1>
-            <h1>Sign Up</h1>
+            <h1 
+            :class="{ active: activeForm === 'login' }"
+            @click="activeForm = 'login'">Login</h1>
+            <h1 
+            :class="{ active: activeForm === 'signup' }"
+            @click="activeForm = 'signup'">Sign Up</h1>
         </header>
-        <form class="Login">
+
+        <!-- LOGIN -->
+        <form v-if="activeForm === 'login'" class="Login">
             <ul>
                 <li>
                     <img />
-                    <input type="text" id="username" name="username">
+                    <input type="text" id="username" name="username" placeholder="Inserisci il nome utente">
                 </li>
                 <li>
                     <img />
-                    <input type="text" id="password" name="password"> 
+                    <input type="text" id="password" name="password" placeholder="Inserisci la Password">
                 </li>
             </ul>
             <input type="submit" value="Submit">
         </form>
-        <form class="SignUp">
+
+        <!-- SIGN UP -->
+        <form v-if="activeForm === 'signup'" class="SignUp">
             <ul>
                 <li>
                     <img />
-                    <input type="text" id="username" name="username">
+                    <input type="text" id="username" name="username" placeholder="Inserisci il nome utente">
                 </li>
                 <li>
                     <img />
-                    <input type="text" id="password" name="password"> 
+                    <input type="text" id="password" name="password" placeholder="Inserisci la Password"> 
+                </li>
+                <li>
+                    <img />
+                    <input type="text" id="confirm_password" name="confirm_password" placeholder="Conferma Password"> 
                 </li>
             </ul>
             <input type="submit" value="Submit">
         </form>
     </div>
 </template>
+
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      activeForm: "login" as "login" | "signup",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  cursor: pointer;
+  opacity: 0.5;
+}
+
+h1.active {
+  opacity: 1;
+  text-decoration: underline;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
