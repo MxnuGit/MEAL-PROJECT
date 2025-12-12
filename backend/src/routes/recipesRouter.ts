1d209c0dde8291d4468b31829f4f2a1f48cfb68e
Menghetz <manuelmenghetti0@gmail.com>
2025-12-12 17:55:27 +0100

    minor fixes

--- backend/src/routes/recipesRouter.ts
+++ backend/src/routes/recipesRouter.ts
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import * as recipeController from "../controller/recipesController"
+import * as recipesController from "../controller/recipesController"
 
 const router: Router = Router()
 
-<<<<<<< HEAD
-router.get("/api/recipe", recipeController.allRecipes)
-=======
-router.get("/ricette/:recipeName", ricetteController.recipeByName)
->>>>>>> 33a01204e9dd170372ddd6f026f90d96efed2f59
+router.get("/recipe/:recipeName", recipesController.recipeByName)
 
 export default router
