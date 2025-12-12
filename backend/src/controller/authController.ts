09eadef31daace67c7e68324b7796f9adf7ce7c5
Menghetz <manuelmenghetti0@gmail.com>
2025-12-12 15:53:42 +0100

    Add apis for authentication

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from "express";
-import { User } from "../utils/auth"
+import { getUser, setUser, unsetUser, User } from "../utils/auth"
 import { connection } from "../utils/db"
+import bcrypt from "bcrypt"
 
 export const register = async (req: Request, res: Response) => {
+    const user = getUser(req, res)
+    if(user) {
+        res.status(401).send("Questa operazione richiede il logout")
+        return
+    }
+    
     const { username, password } = req.body
+
+    const [users] = await connection.execute("SELECT username FROM users WHERE username=?", [username])
+
+    if(Array.isArray(users) && users.length > 0) {
+        res.status(400).send("Username già in uso")
+        return 
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
 
     await connection.execute("INSERT INTO users (username, password) VALUES (?, ?)", [
         username,
-        password,
+        passwordHash,
     ])
 
     const [results] = await connection.execute(
@@ -16,10 +32,18 @@ export const register = async (req: Request, res: Response) => {
     )
     const newUser = (results as User[])[0]
 
+    setUser(req, res, newUser)
+
     res.json({ message: "Registrazione effetuata con successo" })
 }
 
 export const login = async (req: Request, res: Response) => {
+    const user = getUser(req, res)
+    if(user) {
+        res.status(401).send("Questa operazione richiede il logout")
+        return
+    }
+
     const { username, password } = req.body
 
     const [results] = await connection.execute(
@@ -27,13 +51,39 @@ export const login = async (req: Request, res: Response) => {
         [username]
     )
 
+    if(!Array.isArray(results) || results.length == 0){
+        res.status(400).send("Credenziali errate")
+        return
+    }
+
+    const userData = results[0] as any
+
+    const correctPassword = await bcrypt.compare(password, userData.password)
+
+    if(!correctPassword) {
+        res.status(400).send("Credenziali errate")
+        return
+    }
+
+    delete userData.password
+
+    setUser(req, res, userData)
+
     res.json({ message: "Login effettuato con successo" })
 }
 
 export const logout = async (req: Request, res: Response) => {
+    const user = getUser(req, res)
+    if(!user) {
+        res.status(401).send("Questa operazione richiede il login")
+        return
+    }
+
+    unsetUser(req, res)
     res.json({ message: "Logout effettuato con successo" })
 }
 
 export const getProfile = async (req: Request, res: Response) => {
+    const user = getUser(req, res)
     res.json({})
 }
--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -12,8 +12,25 @@ const COOKIE_NAME = "meal-access-token"
 
 export const setUser = (req: Request, res: Response, user: any) => {
     const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: 86_400_000})
+    res.cookie(COOKIE_NAME, accessToken, {
+        maxAge: 86_400_000,
+        httpOnly: true,
+        sameSite: true,
+        // secure: true
+    })
 }
 
-export const getUser = (req: Request, res: Response) => {}
+export const getUser = (req: Request, res: Response) => {
+    const accessToken = req.cookies[COOKIE_NAME]
+    if(!accessToken) return null
+    try {
+        const user = jwt.verify(accessToken, JWT_SECRET) as User
+        return user
+    } catch {
+        return null
+    }
+}
 
-export const unsetUser = (req: Request, res: Response) => {}
+export const unsetUser = (req: Request, res: Response) => {
+    res.clearCookie(COOKIE_NAME)
+}
