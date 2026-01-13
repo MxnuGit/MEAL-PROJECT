3918e8a5f25f11e9732babe2da727e2dbef261e1
Menghetz <manuelmenghetti0@gmail.com>
2026-01-13 16:01:58 +0100

    add route

--- backend/src/routes/recipesRouter.ts
+++ backend/src/routes/recipesRouter.ts
@@ -10,6 +10,8 @@ router.get("/api/users/:username/recipes", recipesController.recipesByUsername);
 router.get("/api/recipes/:id", recipesController.recipesByID);
 router.get("/api/recipes/:id/ingredients", recipesController.ingredientsByID);
 router.get("/api/recipes/:id/steps", recipesController.stepsByID)
+router.get("/api/recipe/searchByIngredients", recipesController.recipesByIngredients);
+
 router.post("/api/createRecipe", recipesController.createRecipe);
 
 router.delete("/api/deleteRecipe/:id", recipesController.deleteRecipe);
